235311f2ed79db8ab5cd9de36571a794452ee761
ceplusplusgithub <123546537+ceplusplusgithub@users.noreply.github.com>
2023-09-24 10:42:12 +0200

    Update Digital_Feed_7e2.ino

--- Digital_Feed_7e2/Digital_Feed_7e2.ino
+++ Digital_Feed_7e2/Digital_Feed_7e2.ino
@@ -6,7 +6,7 @@
 
 #define ENC_LINE_PER_REV     3600      // Encoder lines per 1 spindle turn (encoder counts 1800 multiple 2:1 belt drive makes 3600 results in 7200 ticks per rev)
 #define MOTOR_Z_STEP_PER_REV 200      // Z motor steps (leadscrew) - almost always 200, microsteps configured below
-#define SCREW_Z              96      // Z (leadscrew) pitch, in hundreds of a mm (pitch is 5mm, but there is a 1:5 gear in front, minus 4 correction empirically)
+#define SCREW_Z              500      // Z (leadscrew) pitch, in hundreds of a mm (was: 96 pitch is 5mm, but there is a 1:5 gear in front, minus 4 correction empirically)
 #define McSTEP_Z             4        // Z driver microsteps (400 steps = 2, 800 steps = 4, etc)
 #define Z_INVERT                      // Comment this line if Z motor is connected directly to the lead screw, uncomment if via belt
 // #define Z_ENA_INVERT                  // Comment/uncomment this line if Z motor is getting disabled when it should be enabled
@@ -14,13 +14,13 @@
 #define SCREW_X              400       // X (cross-slide) pitch, in hundreds of a mm
 #define REBOUND_X            0      // X backlash in microsteps
 #define REBOUND_Z            0      // Z backlash in microsteps
-#define McSTEP_X             8        // X driver microsteps (400 steps = 2, 800 steps = 4, etc)
+#define McSTEP_X             4        // X driver microsteps (400 steps = 2, 800 steps = 4, etc)
 //
 #define THRD_ACCEL           25       // K. division from which we will accelerate on Threads, Accel+Ks should be < 255
 #define FEED_ACCEL           3        // Rigidity of acceleration at feeds, more value - shorter acceleration.
 //
-#define MIN_FEED             2        // Min feed in 0.01mm. 2 = 0.02mm
-#define MAX_FEED             20       // Max feed in 0.01mm. 20 = 0.20mm
+#define MIN_FEED             8        // Min feed in 0.01mm. 2 = 0.02mm
+#define MAX_FEED             40       // Max feed in 0.01mm. 20 = 0.20mm
 #define MIN_aFEED            20       // Min feed in mm/min. 20 = 20mm/min
 #define MAX_aFEED            400      // Max feed in mm/min. 400 = 400mm/min
 
@@ -35,31 +35,29 @@
 #define HC_START_SPEED_1     250      // MPG start, 250000/(250+1)/800*60/2 = 37rpm
 #define HC_MAX_SPEED_1       150      // maximum MPG speed, 250000/(150+1)/800*60/2 = 62rpm
 #define HC_START_SPEED_10    150      // MPG start, 250000/(150+1)/800*60/2 = 62rpm
-#define HC_MAX_SPEED_10      23       // maximum MPG speed, 250000/(23+1)/800*60/2 = 391rpm
+#define HC_MAX_SPEED_10      48       // maximum MPG speed, 250000/(23+1)/800*60/2 = 391rpm
 #define HC_X_DIR             0        // 1-CW, 0-CCW
 #define HC_Z_DIR             0        // 1-CW, 0-CCW
 
 
 //////////////////////////////////////////////////////////////////////////////////////////////////
 #define a  (uint32_t)(ENC_LINE_PER_REV / ((float)MOTOR_Z_STEP_PER_REV * McSTEP_Z * MIN_FEED / SCREW_Z) /2 +0.5)
-//
-static_assert(a <= 255, "Invalid value MIN_FEED");
+static_assert(a <= 255, "Invalid value MIN_FEED Axis Z");
 #define b  (uint32_t)(ENC_LINE_PER_REV / ((float)MOTOR_Z_STEP_PER_REV * McSTEP_Z * MAX_FEED / SCREW_Z) /2 +0.5)
 static_assert(b > 1, "Invalid value MAX_FEED Axis Z");
 #define c  250000 / ((uint32_t)MIN_aFEED * MOTOR_Z_STEP_PER_REV * McSTEP_Z / ((uint32_t)60 * SCREW_Z / 100) * 2) -1
-static_assert(c <= 65535, "Invalid value MIN_aFEED");
+static_assert(c <= 65535, "Invalid value MIN_aFEED Axis Z");
 #define d  250000 / ((uint32_t)MAX_aFEED * MOTOR_Z_STEP_PER_REV * McSTEP_Z / ((uint32_t)60 * SCREW_Z / 100) * 2) -1
-static_assert(d > 1, "Invalid value MAX_aFEED");
+static_assert(d > 1, "Invalid value MAX_aFEED Axis Z");
 
 #define e  (uint32_t)(ENC_LINE_PER_REV / ((float)MOTOR_X_STEP_PER_REV * McSTEP_X * MIN_FEED / SCREW_X) /2 +0.5)
-//
-static_assert(e <= 255, "Invalid value MIN_FEED");
+static_assert(e <= 255, "Invalid value MIN_FEED Axis X");
 #define f  (uint32_t)(ENC_LINE_PER_REV / ((float)MOTOR_X_STEP_PER_REV * McSTEP_X * MAX_FEED / SCREW_X) /2 +0.5)
 static_assert(f > 1, "Invalid value MAX_FEED Axis X");
 #define g  250000 / ((uint32_t)MIN_aFEED * MOTOR_X_STEP_PER_REV * McSTEP_X / ((uint32_t)60 * SCREW_X / 100) * 2) -1
-static_assert(g <= 65535, "Invalid value MIN_aFEED");
+static_assert(g <= 65535, "Invalid value MIN_aFEED  Axis X");
 #define h  250000 / ((uint32_t)MAX_aFEED * MOTOR_X_STEP_PER_REV * McSTEP_X / ((uint32_t)60 * SCREW_X / 100) * 2) -1
-static_assert(h > 1, "Invalid value MAX_aFEED");
+static_assert(h > 1, "Invalid value MAX_aFEED  Axis X");
 //////////////////////////////////////////////////////////
 
 
@@ -286,31 +284,55 @@ struct cone_info_type
 };
 // This table must be re-calculated each time hardware parameters
 //  at the top change. Use tables.html to find the new values.
-const cone_info_type Cone_Info[] = {
-  {  8, 3333, "45dg"},
-  {320, 2000, " KM0"}, // 1:19.212
-  {334, 1167, " KM1"}, // 1:20.047
-  {333, 6667, " KM2"}, // 1:20.02
-  {332,  333, " KM3"}, // 1:19.922
-  {320, 9000, " KM4"}, // 1:19.254
-  {316, 7000, " KM5"}, // 1:19.002
-  {319, 6667, " KM6"}, // 1:19.18
-  { 66, 6667, " 1:4"}, // 1:4
-  { 83, 3333, " 1:5"}, // 1:5
-  {116, 6667, " 1:7"}, // 1:7
-  {166, 6667, "1:10"}, // 1:10
-  {266, 6667, "1:16"}, // 1:16
-  {333, 3333, "1:20"}, // 1:20
-  {400,    0, "1:24"}, // 1:24
-  {500,    0, "1:30"}, // 1:30
-  {833, 3333, "1:50"}, // 1:50
-  {152, 3810, "7:64"}, // 7:64
-  { 59, 2947, " 8dg"},
-  { 47, 2607, "10dg"},
-  { 31, 1004, "15dg"},
-  { 14, 4338, "30dg"},
-};
+// const cone_info_type Cone_Info[] = {
+//   {  8, 3333, "45dg"},
+//   {320, 2000, " KM0"}, // 1:19.212
+//   {334, 1167, " KM1"}, // 1:20.047
+//   {333, 6667, " KM2"}, // 1:20.02
+//   {332,  333, " KM3"}, // 1:19.922
+//   {320, 9000, " KM4"}, // 1:19.254
+//   {316, 7000, " KM5"}, // 1:19.002
+//   {319, 6667, " KM6"}, // 1:19.18
+//   { 66, 6667, " 1:4"}, // 1:4
+//   { 83, 3333, " 1:5"}, // 1:5
+//   {116, 6667, " 1:7"}, // 1:7
+//   {166, 6667, "1:10"}, // 1:10
+//   {266, 6667, "1:16"}, // 1:16
+//   {333, 3333, "1:20"}, // 1:20
+//   {400,    0, "1:24"}, // 1:24
+//   {500,    0, "1:30"}, // 1:30
+//   {833, 3333, "1:50"}, // 1:50
+//   {152, 3810, "7:64"}, // 7:64
+//   { 59, 2947, " 8dg"},
+//   { 47, 2607, "10dg"},
+//   { 31, 1004, "15dg"},
+//   { 14, 4338, "30dg"},
+// };
 
+const cone_info_type Cone_Info[] = {
+  {  1, 6000, "45dg"},
+  { 61, 4784, " KM0"}, // 1:19.212
+  { 64, 1504, " KM1"}, // 1:20.047
+  { 64,  640, " KM2"}, // 1:20.02
+  { 63, 7504, " KM3"}, // 1:19.922
+  { 61, 6128, " KM4"}, // 1:19.254
+  { 60, 8064, " KM5"}, // 1:19.002
+  { 61, 3760, " KM6"}, // 1:19.18
+  { 12, 8000, " 1:4"}, // 1:4
+  { 16,    0, " 1:5"}, // 1:5
+  { 22, 4000, " 1:7"}, // 1:7
+  { 32,    0, "1:10"}, // 1:10
+  { 51, 2000, "1:16"}, // 1:16
+  { 64,    0, "1:20"}, // 1:20
+  { 76, 8000, "1:24"}, // 1:24
+  { 95, 10000, "1:30"}, // 1:30
+  {160,    0, "1:50"}, // 1:50
+  { 29, 2571, "7:64"}, // 7:64
+  { 11, 3846, " 8dg"},
+  {  9,  741, "10dg"},
+  {  5, 9713, "15dg"},
+  {  2, 7713, "30dg"},
+};
 
 
 #define TOTAL_CONE (sizeof(Cone_Info) / sizeof(Cone_Info[0]))
@@ -330,51 +352,99 @@ struct thread_info_type
 };
 // This table must be re-calculated each time hardware parameters
 //  at the top change. Use tables.html to find the new values.
-const thread_info_type Thread_Info[] = {
-  { 34, 5600, 144,    0, "0.25mm", 0.250, 4, " 750rpm"},
-  { 28, 8000, 120,    0, "0.30mm", 0.300, 4, " 750rpm"},
-  { 24, 6857, 102, 8571, "0.35mm", 0.350, 4, " 750rpm"},
-  { 21, 6000,  90,    0, "0.40mm", 0.400, 4, " 750rpm"},
-  { 17, 2800,  72,    0, "0.50mm", 0.500, 4, " 750rpm"},
-  { 14, 4000,  60,    0, "0.60mm", 0.600, 4, " 750rpm"},
-  { 12, 3429,  51, 4286, "0.70mm", 0.700, 4, " 750rpm"},
-  { 11, 5200,  48,    0, "0.75mm", 0.750, 5, " 750rpm"},
-  { 10, 8000,  45,    0, "0.80mm", 0.800, 5, " 700rpm"},
-  {  8, 6400,  36,    0, "1.00mm", 1.000, 6, " 560rpm"},
-  {  6, 9120,  28, 8000, "1.25mm", 1.250, 7, " 460rpm"},
-  {  5, 7600,  24,    0, "1.50mm", 1.500, 7, " 380rpm"},
-  {  4, 9371,  20, 5714, "1.75mm", 1.750, 8, " 320rpm"},
-  {  4, 3200,  18,    0, "2.00mm", 2.000, 9, " 280rpm"},
-  {  3, 4560,  14, 4000, "2.50mm", 2.500, 11, " 220rpm"},
-  {  2, 8800,  12,    0, "3.00mm", 3.000, 15, " 190rpm"},
-  {  2, 1600,   9,    0, "4.00mm", 4.000, 22, " 140rpm"},
+// const thread_info_type Thread_Info[] = {
+//   { 34, 5600, 144,    0, "0.25mm", 0.250, 4, " 750rpm"},
+//   { 28, 8000, 120,    0, "0.30mm", 0.300, 4, " 750rpm"},
+//   { 24, 6857, 102, 8571, "0.35mm", 0.350, 4, " 750rpm"},
+//   { 21, 6000,  90,    0, "0.40mm", 0.400, 4, " 750rpm"},
+//   { 17, 2800,  72,    0, "0.50mm", 0.500, 4, " 750rpm"},
+//   { 14, 4000,  60,    0, "0.60mm", 0.600, 4, " 750rpm"},
+//   { 12, 3429,  51, 4286, "0.70mm", 0.700, 4, " 750rpm"},
+//   { 11, 5200,  48,    0, "0.75mm", 0.750, 5, " 750rpm"},
+//   { 10, 8000,  45,    0, "0.80mm", 0.800, 5, " 700rpm"},
+//   {  8, 6400,  36,    0, "1.00mm", 1.000, 6, " 560rpm"},
+//   {  6, 9120,  28, 8000, "1.25mm", 1.250, 7, " 460rpm"},
+//   {  5, 7600,  24,    0, "1.50mm", 1.500, 7, " 380rpm"},
+//   {  4, 9371,  20, 5714, "1.75mm", 1.750, 8, " 320rpm"},
+//   {  4, 3200,  18,    0, "2.00mm", 2.000, 9, " 280rpm"},
+//   {  3, 4560,  14, 4000, "2.50mm", 2.500, 11, " 220rpm"},
+//   {  2, 8800,  12,    0, "3.00mm", 3.000, 15, " 190rpm"},
+//   {  2, 1600,   9,    0, "4.00mm", 4.000, 22, " 140rpm"},
+//   {  1, 7280,   7, 2000, "5.00mm", 5.000, 24, " 100rpm"},
 
-  { 27, 2126, 113, 3858, "80tpi ", 0.318, 4, " 750rpm"},
-  { 24, 4913, 102,  472, "72tpi ", 0.353, 4, " 750rpm"},
-  { 21, 7701,  90, 7087, "64tpi ", 0.397, 4, " 750rpm"},
-  { 20, 4094,  85,  394, "60tpi ", 0.423, 4, " 750rpm"},
-  { 19,  488,  79, 3701, "56tpi ", 0.454, 4, " 750rpm"},
-  { 16, 3276,  68,  315, "48tpi ", 0.529, 4, " 750rpm"},
-  { 14, 9669,  62, 3622, "44tpi ", 0.577, 4, " 750rpm"},
-  { 13, 6063,  56, 6929, "40tpi ", 0.635, 4, " 750rpm"},
-  { 12, 2457,  51,  236, "36tpi ", 0.706, 5, " 750rpm"},
-  { 10, 8850,  45, 3543, "32tpi ", 0.794, 5, " 710rpm"},
-  {  9, 5244,  39, 6850, "28tpi ", 0.907, 5, " 650rpm"},
-  {  9, 1843,  38, 2677, "27tpi ", 0.941, 5, " 600rpm"},
-  {  8, 8441,  36, 8504, "26tpi ", 0.977, 6, " 570rpm"},
-  {  8, 1638,  34,  157, "24tpi ", 1.058, 6, " 500rpm"},
-  {  7, 4835,  31, 1811, "22tpi ", 1.155, 6, " 450rpm"},
-  {  6, 8031,  28, 3465, "20tpi ", 1.270, 7, " 440rpm"},
-  {  6, 4630,  26, 9291, "19tpi ", 1.337, 7, " 420rpm"},
-  {  6, 1228,  25, 5118, "18tpi ", 1.411, 7, " 380rpm"},
-  {  5, 4425,  22, 6772, "16tpi ", 1.587, 8, " 350rpm"},
-  {  4, 7622,  19, 8425, "14tpi ", 1.814, 9, " 320rpm"},
-  {  4,  819,  17,   79, "12tpi ", 2.117, 10, " 270rpm"},
-  {  3, 4016,  14, 1732, "10tpi ", 2.540, 11, " 220rpm"},
-  {  3,  614,  12, 7559, " 9tpi ", 2.822, 14, " 190rpm"},
-  {  2, 7213,  11, 3386, " 8tpi ", 3.175, 16, " 170rpm"},
-  {  2, 3811,   9, 9213, " 7tpi ", 3.629, 19, " 150rpm"},
-  {  2,  409,   8, 5039, " 6tpi ", 4.233, 24, " 140rpm"},
+//   { 27, 2126, 113, 3858, "80tpi ", 0.318, 4, " 750rpm"},
+//   { 24, 4913, 102,  472, "72tpi ", 0.353, 4, " 750rpm"},
+//   { 21, 7701,  90, 7087, "64tpi ", 0.397, 4, " 750rpm"},
+//   { 20, 4094,  85,  394, "60tpi ", 0.423, 4, " 750rpm"},
+//   { 19,  488,  79, 3701, "56tpi ", 0.454, 4, " 750rpm"},
+//   { 16, 3276,  68,  315, "48tpi ", 0.529, 4, " 750rpm"},
+//   { 14, 9669,  62, 3622, "44tpi ", 0.577, 4, " 750rpm"},
+//   { 13, 6063,  56, 6929, "40tpi ", 0.635, 4, " 750rpm"},
+//   { 12, 2457,  51,  236, "36tpi ", 0.706, 5, " 750rpm"},
+//   { 10, 8850,  45, 3543, "32tpi ", 0.794, 5, " 710rpm"},
+//   {  9, 5244,  39, 6850, "28tpi ", 0.907, 5, " 650rpm"},
+//   {  9, 1843,  38, 2677, "27tpi ", 0.941, 5, " 600rpm"},
+//   {  8, 8441,  36, 8504, "26tpi ", 0.977, 6, " 570rpm"},
+//   {  8, 1638,  34,  157, "24tpi ", 1.058, 6, " 500rpm"},
+//   {  7, 4835,  31, 1811, "22tpi ", 1.155, 6, " 450rpm"},
+//   {  6, 8031,  28, 3465, "20tpi ", 1.270, 7, " 440rpm"},
+//   {  6, 4630,  26, 9291, "19tpi ", 1.337, 7, " 420rpm"},
+//   {  6, 1228,  25, 5118, "18tpi ", 1.411, 7, " 380rpm"},
+//   {  5, 4425,  22, 6772, "16tpi ", 1.587, 8, " 350rpm"},
+//   {  4, 7622,  19, 8425, "14tpi ", 1.814, 9, " 320rpm"},
+//   {  4,  819,  17,   79, "12tpi ", 2.117, 10, " 270rpm"},
+//   {  3, 4016,  14, 1732, "10tpi ", 2.540, 11, " 220rpm"},
+//   {  3,  614,  12, 7559, " 9tpi ", 2.822, 14, " 190rpm"},
+//   {  2, 7213,  11, 3386, " 8tpi ", 3.175, 16, " 170rpm"},
+//   {  2, 3811,   9, 9213, " 7tpi ", 3.629, 19, " 150rpm"},
+//   {  2,  409,   8, 5039, " 6tpi ", 4.233, 24, " 140rpm"},
+// };
+
+const thread_info_type Thread_Info[] = {
+  {720,    0, 576,    0, "0.25mm", 0.250, 4, " 750rpm"},
+  {600,    0, 480,    0, "0.30mm", 0.300, 4, " 750rpm"},
+  {514, 2857, 411, 4286, "0.35mm", 0.350, 4, " 750rpm"},
+  {450,    0, 360,    0, "0.40mm", 0.400, 4, " 750rpm"},
+  {360,    0, 288,    0, "0.50mm", 0.500, 4, " 750rpm"},
+  {300,    0, 240,    0, "0.60mm", 0.600, 4, " 750rpm"},
+  {257, 1429, 205, 7143, "0.70mm", 0.700, 4, " 750rpm"},
+  {240,    0, 192,    0, "0.75mm", 0.750, 5, " 750rpm"},
+  {225,    0, 180,    0, "0.80mm", 0.800, 5, " 700rpm"},
+  {180,    0, 144,    0, "1.00mm", 1.000, 6, " 560rpm"},
+  {144,    0, 115, 2000, "1.25mm", 1.250, 7, " 460rpm"},
+  {120,    0,  96,    0, "1.50mm", 1.500, 7, " 380rpm"},
+  {102, 8571,  82, 2857, "1.75mm", 1.750, 8, " 320rpm"},
+  { 90,    0,  72,    0, "2.00mm", 2.000, 9, " 280rpm"},
+  { 72,    0,  57, 6000, "2.50mm", 2.500, 11, " 220rpm"},
+  { 60,    0,  48,    0, "3.00mm", 3.000, 15, " 190rpm"},
+  { 45,    0,  36,    0, "4.00mm", 4.000, 22, " 140rpm"},
+
+  {566, 9291, 453, 5433, "80tpi ", 0.318, 4, " 750rpm"},
+  {510, 2362, 408, 1890, "72tpi ", 0.353, 4, " 750rpm"},
+  {453, 5433, 362, 8346, "64tpi ", 0.397, 4, " 750rpm"},
+  {425, 1969, 340, 1575, "60tpi ", 0.423, 4, " 750rpm"},
+  {396, 8504, 317, 4803, "56tpi ", 0.454, 4, " 750rpm"},
+  {340, 1575, 272, 1260, "48tpi ", 0.529, 4, " 750rpm"},
+  {311, 8110, 249, 4488, "44tpi ", 0.577, 4, " 750rpm"},
+  {283, 4646, 226, 7717, "40tpi ", 0.635, 4, " 750rpm"},
+  {255, 1181, 204,  945, "36tpi ", 0.706, 5, " 750rpm"},
+  {226, 7717, 181, 4173, "32tpi ", 0.794, 5, " 710rpm"},
+  {198, 4252, 158, 7402, "28tpi ", 0.907, 5, " 650rpm"},
+  {191, 3386, 153,  709, "27tpi ", 0.941, 5, " 600rpm"},
+  {184, 2520, 147, 4016, "26tpi ", 0.977, 6, " 570rpm"},
+  {170,  787, 136,  630, "24tpi ", 1.058, 6, " 500rpm"},
+  {155, 9055, 124, 7244, "22tpi ", 1.155, 6, " 450rpm"},
+  {141, 7323, 113, 3858, "20tpi ", 1.270, 7, " 440rpm"},
+  {134, 6457, 107, 7165, "19tpi ", 1.337, 7, " 420rpm"},
+  {127, 5591, 102,  472, "18tpi ", 1.411, 7, " 380rpm"},
+  {113, 3858,  90, 7087, "16tpi ", 1.587, 8, " 350rpm"},
+  { 99, 2126,  79, 3701, "14tpi ", 1.814, 9, " 320rpm"},
+  { 85,  394,  68,  315, "12tpi ", 2.117, 10, " 270rpm"},
+  { 70, 8661,  56, 6929, "10tpi ", 2.540, 11, " 220rpm"},
+  { 63, 7795,  51,  236, " 9tpi ", 2.822, 14, " 190rpm"},
+  { 56, 6929,  45, 3543, " 8tpi ", 3.175, 16, " 170rpm"},
+  { 49, 6063,  39, 6850, " 7tpi ", 3.629, 19, " 150rpm"},
+  { 42, 5197,  34,  157, " 6tpi ", 4.233, 24, " 140rpm"},
 };
 #define TOTAL_THREADS (sizeof(Thread_Info) / sizeof(Thread_Info[0]))
 #define PASS_FINISH   3 // THRD_PS_FN ???
@@ -410,7 +480,7 @@ const thread_info_type Thread_Info[] = {
 
 #define Enable_INT_OCR4B()    do {TCNT4 = 0; TIFR4 = (1<<OCF4B); TIMSK4 = (1<<OCIE4B);} while(0)
 #define Disable_INT_OCR4B()       TIMSK4 &= ~(1<<OCIE4B)
-
+// setzt den Takt für den asynchronen Vorschub?
 #define Enable_INT_OCR5A()    do {TCNT5 = 0; TIFR5 = (1<<OCF5A); TIMSK5 = (1<<OCIE5A);} while(0)
 #define Disable_INT_OCR5A()       TIMSK5 &= ~(1<<OCIE5A)
 
@@ -571,6 +641,7 @@ byte Sub_Mode_Feed = Sub_Mode_Feed_Man;
 byte Sub_Mode_aFeed = Sub_Mode_aFeed_Man;
 byte Sub_Mode_Cone = Sub_Mode_Cone_Man;
 byte Sub_Mode_Sphere = Sub_Mode_Sphere_Man;
+// The n-th configuration set of the ThreadInfo Array, not a physical step
 byte Thread_Step = 11;
 byte Cone_Step = 0;
 
@@ -588,7 +659,7 @@ uint16_t Feed_mm = 0;
 uint16_t aFeed_mm = 0;
 
 uint16_t Start_Speed = ENC_LINE_PER_REV / ((uint32_t)MOTOR_Z_STEP_PER_REV * McSTEP_Z * MIN_FEED / SCREW_Z) /FEED_ACCEL;
-uint16_t max_OCR5A = ENC_LINE_PER_REV / ((uint32_t)MOTOR_Z_STEP_PER_REV * McSTEP_Z * MIN_FEED / SCREW_Z) /FEED_ACCEL;                                                                                     // Ð�Ð°Ñ‡Ð°Ð»ÑŒÐ½Ð°Ñ� Ñ�ÐºÐ¾Ñ€Ð¾Ñ�Ñ‚ÑŒ Ð¿Ð¾Ð´Ð°Ñ‡Ð¸ Ð¿Ñ€Ð¸ Ñ€Ð°Ð·Ð³Ð¾Ð½Ðµ/Ñ‚Ð¾Ñ€Ð¼Ð¾Ð¶ÐµÐ½Ð¸Ð¸
+uint16_t max_OCR5A = ENC_LINE_PER_REV / ((uint32_t)MOTOR_Z_STEP_PER_REV * McSTEP_Z * MIN_FEED / SCREW_Z) /FEED_ACCEL;
 uint16_t max_OCR4A = (250000 / ((uint32_t)MIN_aFEED * MOTOR_Z_STEP_PER_REV * McSTEP_Z / ((uint32_t)60 * SCREW_Z / 100) * 2) - 1) /FEED_ACCEL;
 
 byte Total_Tooth = 1;
